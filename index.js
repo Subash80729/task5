524761eff75e68332816021caf726a9271f7acbc
Subash80729 <108263629+Subash80729@users.noreply.github.com>
2022-07-29 23:17:31 +0530

    File uploaded

---
+++ index.js
@@ -0,0 +1,91 @@
+//console.log("I'm here")
+
+//var name="subash";
+//var name="manoj"
+//console.log(name)
+//let name="subash";
+//let username="ram";
+ //console.log(username)
+ var name="subash"
+ var num=23
+ var isAvail=true
+ var nodata=undefined
+
+//  console.log(typeof(nodata))
+var var1="subash"
+var var1="manoj"
+var num=30
+var isAvail=true
+var myData=undefined
+var obj = {id: 1, name: "suresh",age: 30,isDataAvail: true,myData: [23333,554]}//obj{key:value}
+var name1= ["ram","rem","ffm"]
+//  console.log(name1[1])
+//operators
+var val1=30
+var val2=20
+sum=val1+val2
+sub=val1-val2
+mul=val1*val2
+div=val1/val2
+var increment=val1++
+var increment1=++val1
+var decrement=val2--
+var decrement1=--val2
+
+// console.log(sum)
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
+// console.log(increment1)
+// console.log(increment)
+// console.log(decrement)
+// console.log(decrement1)
+
+
+//Assignment operators
+
+var val3=33
+var val4=2
+val4 += 40;
+val3 -= 9;
+val4 *=6;
+val3 /= 3;
+
+// console.log(val4)
+// console.log(val3)
+
+//String operator
+
+var a="subash"
+
+a += "g";
+// console.log(a)
+
+//comparison operator
+
+var valb=50
+var vala=30
+
+// var result= vala !=valb
+// var result1 = valb<=vala
+
+var output = vala <=50 ? 'vala is greater':'vala is smaller'
+// console.log(result)
+// console.log(result1)
+// console.log(output)
+
+//logical operator
+
+var valc=65
+
+var result = valc > 50 && valc ==50 ? 'okkkkk':'nooooo'
+var result1 = valc > 50 || valc ==50 ? 'okkkkk':'nooooo'
+var result2 = !valc ? 'good': 'bad'
+
+console.log(result)
+console.log(result1)
+console.log(result2)
+
+
+
+
